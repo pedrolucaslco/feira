2289dcd6e7aa44ae134e06972df5ef53948c8a1c
Pedro Lucas <pedrolucaslcosta@gmail.com>
2026-05-05 00:10:23 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "feira-v54";
+const CACHE_NAME = "feira-v55";
 const ASSETS = ["./", "./index.html", "./assets/app.css", "./styles.css", "./app.js", "./supabase-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
